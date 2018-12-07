21a5adb099da9fe7e92be7b4cad80d600880c88f
siim puustusmaa <siimpuustusmaa@gmail.com>
2018-12-07 21:33:09 +0200

    cert update script

---
+++ nginx/update_lets_encrypt_certificates.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+echo "installing prerequisites"
+
+sudo yum -y install python2-certbot-nginx
+
+SITES=("siimp.ee" "game-of-life.siimp.ee" "nasdaqbaltic.siimp.ee")
+echo "sites are $SITES"
+
+LETS_ENCRYPT_DIR=/etc/letsencrypt/live
+echo "lets encrypt dir ${LETS_ENCRYPT_DIR}"
+
+sudo certbot --nginx certonly
+
+for site in ${SITES[*]}
+do
+    echo "copying certificates for ${site}"
+    cp "${LETS_ENCRYPT_DIR}/${site}/cert.pem" "/home/sites/${site}/cert/${site}.crt"
+    cp "${LETS_ENCRYPT_DIR}/${site}/privkey.pem" "/home/sites/${site}/cert/${site}.key"
+done
+
+echo "restarting nginx"
+
+sudo service nginx restart
+
+
