14941149f9600b593f1fb1c6856c7d53a6966395
siim puustusmaa <siim.puustusmaa@gmail.com>
2020-07-16 22:23:09 +0300

    updated boids script

--- app/boids/setup.sh
+++ app/boids/setup.sh
@@ -3,5 +3,12 @@
 
 APP_DIR="${APP_DIRECTORY}/boids.siimp.ee"
 echo "--SETTING UP ${APP_DIR}--"
-
 mkdir --parents $APP_DIR
+
+git clone https://github.com/siimp/boids $APP_DIR/git
+mkdir --parents $APP_DIR/static
+cp $APP_DIR/git/*.js $APP_DIR/static
+cp $APP_DIR/git/*.html $APP_DIR/static
+
+cp app/boids/nginx/boids.siimp.ee.conf /etc/nginx/conf.d/
+systemctl reload nginx
