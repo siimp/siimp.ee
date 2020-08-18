9a8549ee7680f82621405a20f0c9572b7f78561b
siim <siimpuustusmaa@gmail.com>
2020-08-18 22:34:32 +0300

    dividend-yields frontend

--- app/dividend-yields/setup.sh
+++ app/dividend-yields/setup.sh
@@ -2,5 +2,14 @@
 
 APP_DIR="${APP_DIRECTORY}/dividend-yields.siimp.ee"
 echo "--SETTING UP ${APP_DIR}--"
+rm -rf $APP_DIR
+mkdir --parents $APP_DIR/git
 
-mkdir --parents $APP_DIR
+git clone https://github.com/siimp/dividend-yields-frontend.git $APP_DIR/git/frontend
+git clone https://github.com/siimp/dividend-yields-scraper.git $APP_DIR/git/backend
+
+./app/dividend-yields/update-frontend.sh 
+
+cp app/dividend-yields/nginx/dividend-yields.siimp.ee.conf /etc/nginx/conf.d/
+
+systemctl reload nginx
