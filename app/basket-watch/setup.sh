744107b496e95e95c100c2d33cdccc9bea0d95e4
siim puustusmaa <siim.puustusmaa@gmail.com>
2020-08-18 21:54:14 +0300

    basket-watch frontend update

--- app/basket-watch/setup.sh
+++ app/basket-watch/setup.sh
@@ -2,10 +2,14 @@
 
 APP_DIR="${APP_DIRECTORY}/basket-watch.siimp.ee"
 echo "--SETTING UP ${APP_DIR}--"
-mkdir --parents $APP_DIR
+rm -rf $APP_DIR
+mkdir --parents $APP_DIR/git
 
-git clone https://github.com/siimp/basket-watch.git
+git clone https://github.com/siimp/basket-watch.git $APP_DIR/git/frontend
+git clone https://github.com/siimp/basket-watch-backend.git $APP_DIR/git/backend
 
-cp app/basket-watch/nginx/basket-watch.ee.conf /etc/nginx/conf.d/
+./app/basket-watch/update-frontend.sh 
+
+cp app/basket-watch/nginx/basket-watch.siimp.ee.conf /etc/nginx/conf.d/
 
 systemctl reload nginx
---
+++ app/basket-watch/update-frontend.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+APP_DIR="${APP_DIRECTORY}/basket-watch.siimp.ee"
+echo "--UPDATING ${APP_DIR} FRONTEND--"
+
+rm -rf $APP_DIR/static 
+mkdir --parent $APP_DIR/static
+
+BUILD_DIR="$APP_DIR/git"
+CURRENT_DIR=$(pwd)
+
+# Frontend
+cd $BUILD_DIR/frontend
+git pull
+npm install
+npm run build
+cp -r dist/* $APP_DIR/static
+cd $CURRENT_DIR
+
+systemctl reload nginx
+
