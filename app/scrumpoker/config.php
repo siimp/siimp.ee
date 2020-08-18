8c2fa9a041db42fdd24c14e8c2b8d334b64816b7
siim <siimpuustusmaa@gmail.com>
2020-08-18 23:31:34 +0300

    scrumpoker

---
+++ app/scrumpoker/config.php
@@ -0,0 +1,43 @@
+?php
+// database configuration parameters
+$conn = array(
+    'dbname' => 'scrum_online',
+    'user' => 'root',
+    'password' => 'passwd',
+    'host' => 'localhost',
+    'driver' => 'pdo_mysql',
+);
+
+// This is used to create the join link
+$host = "http://scrumpoker.siimp.ee";
+
+$cardSets = [['1', '2', '3', '5', '8', '13', 'lebo ots', '42']];
+
+// Src tree for documentation linking from page
+$src = "https://github.com/Toxantron/scrumonline/tree/master";
+
+// Active ticketing plugins of the page
+$plugins = [
+    // Plugin to load issues from github
+    'GitHub',
+    // Plugin to load issues from JIRA
+    'JIRA',
+    // Plugin to load issues from Gitlab
+    'Gitlab'
+];
+
+// Configuration for the server side JIRA controller
+$jiraConfiguration = [
+    'base_url' => '',
+    'username' => '',
+    'password' => '',
+    'project' => '',
+    'jql' => '',
+];
+$conn = array(
+    'dbname' => $_ENV["DB_NAME"],
+    'user' => $_ENV["DB_USER"],
+    'password' => $_ENV["DB_PASS"],
+    'host' => $_ENV["DB_HOST"],
+    'driver' => 'pdo_mysql',
+); 
--- app/scrumpoker/setup.sh
+++ app/scrumpoker/setup.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env sh
 
-APP_DIR="${APP_DIRECTORY}/scrumpoker.siimp.ee"
-echo "--SETTING UP ${APP_DIR}--"
+echo "--SETTING UP scrumpoker.siimp.ee--"
+docker run -d -p 8084:80 -e HOST="http://localhost:8084" chrisns/scrumonline:bundled
+
+DOCKER_ID=$(docker ps -aqf "name=containername")
+docker cp app/scrumpoker/config.php $DOCKER_ID:/scrumonline/src/config.php
+docker cp app/scrumpoker/home.php $DOCKER_ID:/scrumonline/src/templates/home.php 
+
 
 cp app/scrumpoker/nginx/scrumpoker.siimp.ee.conf /etc/nginx/conf.d/
 systemctl reload nginx
