f0ef48282c0dbfb8c421108dda29fcd7f76e1ee7
siim <siimpuustusmaa@gmail.com>
2020-08-26 22:35:38 +0300

    basket watch systemd (3)

--- app/basket-watch/update-backend.sh
+++ app/basket-watch/update-backend.sh
@@ -3,5 +3,11 @@
 APP_DIR="${APP_DIRECTORY}/basket-watch.siimp.ee"
 echo "--UPDATING ${APP_DIR} BACKEND--"
 
-chmod +x /app/basket-watch/backend/basket-watch-backend.jar
+rm -rf $APP_DIR/backend
+mkdir --parent $APP_DIR/backend
+cp /home/configurations/basket-watch/application.yml $APP_DIR/backend/application.yml
+cp /home/configurations/basket-watch/basket-watch-backend.jar $APP_DIR/backend/basket-watch-backend.jar
+
+
+chmod +x $APP_DIR/backend/basket-watch-backend.jar
 
