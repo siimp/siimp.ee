8be1e98743cdac824f4cfa79512fd7b57d5d143c
siim <siimpuustusmaa@gmail.com>
2020-08-26 21:19:02 +0300

    basket watch systemd (2)

--- app/basket-watch/setup.sh
+++ app/basket-watch/setup.sh
@@ -8,11 +8,14 @@ mkdir --parents $APP_DIR/git
 git clone https://github.com/siimp/basket-watch.git $APP_DIR/git/frontend
 git clone https://github.com/siimp/basket-watch-backend.git $APP_DIR/git/backend
 
-./app/basket-watch/update-frontend.sh 
 
 cp app/basket-watch/nginx/basket-watch.siimp.ee.conf /etc/nginx/conf.d/
-systemctl reload nginx
+./app/basket-watch/update-frontend.sh 
+
+
+./app/basket-watch/update-backend.sh 
 
 cp app/basket-watch/basket-watch-backend.service /etc/systemd/system
+systemctl daemon-reload
 systemctl enable basket-watch-backend
 systemctl start basket-watch-backend
---
+++ app/basket-watch/update-backend.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+APP_DIR="${APP_DIRECTORY}/basket-watch.siimp.ee"
+echo "--UPDATING ${APP_DIR} BACKEND--"
+
+chmod +x /app/basket-watch/backend/basket-watch-backend.jar
+
