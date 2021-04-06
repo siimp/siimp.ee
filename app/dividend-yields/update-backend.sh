e23866035a82ae7a7a867a4bf326682a6accb97f
siim <siimpuustusmaa@gmail.com>
2021-04-07 00:23:13 +0300

    updated backend update script

--- app/dividend-yields/update-backend.sh
+++ app/dividend-yields/update-backend.sh
@@ -3,11 +3,27 @@
 APP_DIR="${APP_DIRECTORY}/dividend-yields.siimp.ee"
 echo "--UPDATING ${APP_DIR} BACKEND--"
 
+
 rm -rf $APP_DIR/backend
 mkdir --parent $APP_DIR/backend
-cp /home/configurations/dividend-yields/application.properties $APP_DIR/backend/application.properties
-cp /home/configurations/dividend-yields/dividend-yields-backend.jar $APP_DIR/backend/dividend-yields-backend.jar
 
+BUILD_DIR="$APP_DIR/git"
+CURRENT_DIR=$(pwd)
 
+cd $BUILD_DIR/backend
+git pull
+chmod +x gradlew
+./gradlew bootJar
+
+echo "stoping backend..."
+systemctl stop dividend-yields-backend
+sleep 3
+
+cp build/libs/dividend-yields-*.jar $APP_DIR/backend/dividend-yields-backend.jar
 chmod +x $APP_DIR/backend/dividend-yields-backend.jar
+cp /home/configurations/dividend-yields/application.properties $APP_DIR/backend/application.properties
 
+echo "starting backend..."
+systemctl start dividend-yields-backend
+
+cd $CURRENT_DIR
