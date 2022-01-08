9321b67ff421c8d2505c40fbc7f44522c09c7528
Siim Puustusmaa <siimpuustusmaa@gmail.com>
2022-01-08 21:04:57 +0200

    restart parameters and notification for the coding love systemd service

--- app/the-coding-love-random/update.sh
+++ app/the-coding-love-random/update.sh
@@ -9,7 +9,6 @@ cp -rf $APP_DIR/git/* $APP_DIR
 systemctl stop the-coding-love-random
 sleep 1
 systemctl start the-coding-love-random
-systemctl start the-coding-love-random
 
 systemctl reload nginx
 
