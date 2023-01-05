f93e99411b1d11370772d3ee8b772b55c3d66d84
Siim Puustusmaa <siimpuustusmaa@gmail.com>
2023-01-05 23:52:47 +0200

    correct host

--- app/scrumpoker/setup.sh
+++ app/scrumpoker/setup.sh
@@ -3,7 +3,7 @@
 echo "--SETTING UP scrumpoker.siimp.ee--"
 
 docker build -f app/scrumpoker/Dockerfile --tag scrumpocker:latest app/scrumpoker
-docker run -d -p 8084:80 -e HOST="http://localhost:8084" scrumpocker:latest
+docker run -d -p 8084:80 -e HOST="http://scrumpoker.siimp.ee" scrumpocker:latest
 
 cp app/scrumpoker/nginx/scrumpoker.siimp.ee.conf /etc/nginx/conf.d/
 systemctl reload nginx
