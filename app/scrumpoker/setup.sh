03b5b67ca5f38daa0cf8dc5abdabccc9b151139a
Siim Puustusmaa <siimpuustusmaa@gmail.com>
2023-01-05 23:47:06 +0200

    updated setup script

--- app/scrumpoker/setup.sh
+++ app/scrumpoker/setup.sh
@@ -2,7 +2,7 @@
 
 echo "--SETTING UP scrumpoker.siimp.ee--"
 
-docker build --tag scrumpocker:latest .
+docker build -f app/scrumpoker/Dockerfile --tag scrumpocker:latest app/scrumpoker
 docker run -d -p 8084:80 -e HOST="http://localhost:8084" scrumpocker:latest
 
 cp app/scrumpoker/nginx/scrumpoker.siimp.ee.conf /etc/nginx/conf.d/
