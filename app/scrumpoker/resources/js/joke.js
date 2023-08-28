f9987f65d52b586b88b802275b9d232911c0d3c1
Siim Puustusmaa <siimpuustusmaa@gmail.com>
2023-08-28 19:43:27 +0300

    when page ready

--- app/scrumpoker/resources/js/joke.js
+++ app/scrumpoker/resources/js/joke.js
@@ -1,7 +1,10 @@
-const joke = document.getElementById('joke')
-fetch('https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,sexist&type=single')
-	.then(result => {
-		result.json().then(json => {
-			joke.innerHTML = json.joke.replace(/(?:\r\n|\r|\n)/g, '<br>')
-		})
-	})
+window.addEventListener('load', function () {
+    const joke = document.getElementById('joke')
+    fetch('https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,sexist&type=single')
+        .then(result => {
+            result.json().then(json => {
+                joke.innerHTML = json.joke.replace(/(?:\r\n|\r|\n)/g, '<br>')
+            })
+        })
+})
+
