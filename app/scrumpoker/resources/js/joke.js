6f5efd4d35d5381d7cee833b121cd19e388df389
Siim Puustusmaa <siimpuustusmaa@gmail.com>
2023-08-28 19:48:56 +0300

    added stop rm to docker script

--- app/scrumpoker/resources/js/joke.js
+++ app/scrumpoker/resources/js/joke.js
@@ -3,7 +3,8 @@ window.addEventListener('load', function () {
     fetch('https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,sexist&type=single')
         .then(result => {
             result.json().then(json => {
-                joke.innerHTML = json.joke.replace(/(?:\r\n|\r|\n)/g, '<br>')
+                const jokeText = json.joke.replace(/(?:\r\n|\r|\n)/g, '<br>')
+                console.info(jokeText)
             })
         })
 })
