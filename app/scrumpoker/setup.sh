e0d0c6b7449098f39a4e29e797177813a86d205b
Siim Puustusmaa <siimpuustusmaa@gmail.com>
2023-08-28 18:49:48 +0300

    always restart

--- app/scrumpoker/setup.sh
+++ app/scrumpoker/setup.sh
@@ -3,7 +3,7 @@
 echo "--SETTING UP scrumpoker.siimp.ee--"
 
 docker build -f app/scrumpoker/Dockerfile --tag scrumpocker:latest app/scrumpoker
-docker run -d -p 8084:80 -e HOST="http://scrumpoker.siimp.ee" scrumpocker:latest
+docker run -d --restart=always -p 8084:80 -e HOST="http://scrumpoker.siimp.ee" scrumpocker:latest
 
 cp app/scrumpoker/nginx/scrumpoker.siimp.ee.conf /etc/nginx/conf.d/
 systemctl reload nginx
